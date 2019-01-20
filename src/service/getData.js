f2a427fe937b1870822d244bb8d112a60614e15b
ykjver <ykjver@163.com>
2019-01-20 22:33:18 +0800

    update

--- src/service/getData.js
+++ src/service/getData.js
@@ -4,5 +4,7 @@ import fetch from '../config/fetch'
 export const GetAdver = params => fetch('/GetAdver', params, 'get')
 // 1.获取滚动图列表
 export const GetNews = params => fetch('/GetNews', params, 'get')
-//
+// 3.找家政企业
 export const Getcompany = params => fetch('/Getcompany', params, 'get');
+// 4.找家政从业人员
+export const Getpractitioner = params => fetch("/Getpractitioner", params, "get");
