55c2a093366658fb5595abc95c049473029c0795
ykjver <ykjver@163.com>
2019-01-20 22:50:31 +0800

    update

--- src/service/getData.js
+++ src/service/getData.js
@@ -8,3 +8,5 @@ export const GetNews = params => fetch('/GetNews', params, 'get')
 export const Getcompany = params => fetch('/Getcompany', params, 'get');
 // 4.找家政从业人员
 export const Getpractitioner = params => fetch("/Getpractitioner", params, "get");
+// 5.服务项目列表
+export const Getskills = params => fetch("/Getskills", params, "get");
